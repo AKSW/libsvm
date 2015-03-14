0e0c6f9a617fc93c2906d542d2453490af8eed04
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2015-03-14 12:01:55 +0100

    added forgotten attribute to mvn svm_problem.java

--- mvn/src/main/java/semlibsvm/libsvm/svm_problem.java
+++ mvn/src/main/java/semlibsvm/libsvm/svm_problem.java
@@ -1,8 +1,11 @@
 package semlibsvm.libsvm;
 
+import java.util.Map;
+
 public class svm_problem implements java.io.Serializable
 {
 	public int l;
 	public double[] y;
 	public svm_node[][] x;
+    public Map class2id ;
 }
